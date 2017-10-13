5febd38b1b49811ac600001b31a11447ab43a422
IrvinAyala <hypleyone@gmail.com>
2017-10-13 11:38:50 -0600

    funcion agregarCartas

--- scripts/claseCarta.js
+++ scripts/claseCarta.js
@@ -1,17 +1,17 @@
 function Cartas() {
- 
-  this.familia = Math.floor(Math.random()*3);
-  this.numero = Math.floor(Math.random()*13 + 1);
-  this.carta= this.numero + (13 * this.familia);
-    
+
+  this.familia = Math.floor(Math.random() * 3 + 0);
+  this.numero = Math.floor(Math.random() * 13 + 1);
+  this.carta = this.numero + (13 * this.familia);
+
   if (typeof Cartas._iniciado === undefined) {
     Cartas.prototype.generarRuta = function() {
-        
+
       var nombreImagen = "images/" + carta + ".png";
       return nombreImagen;
     };
-  
-    }
+
+  }
   Cartas._iniciado = true;
-  
+
 }
--- scripts/jugador.js
+++ scripts/jugador.js
@@ -3,17 +3,10 @@ var apuestaMinima = 0;
 
 var cartasAsignadas = [];
 
-function jugador(nombre, saldo) {
-    this.jugando = false;
-    this.nombre = nombre;
-    this.saldo = saldo;
-    this.carta1 = new Cartas();
-    this.carta2 = new Cartas();
-}
 
-jugador.prototype.apostar = function (monto) {
-    this.saldo -= monto;
-    };
+jugador.prototype.apostar = function(monto) {
+  this.saldo -= monto;
+};
 
 function jugador(nombre, saldo) {
   this.jugando = false;
@@ -22,112 +15,99 @@ function jugador(nombre, saldo) {
 
 }
 
-jugador.prototype.apostar = function(monto) {
-  saldo -= monto;
-
-};
-
 
 function add(nombre, saldo) {
 
-    var jugadorAgregado = new jugador(nombre, saldo);
-    
-    jugadorAgregado.Carta1 = agregarCarta();
-    jugadorAgregado.Carta2 = agregarCarta();
-    jugadores.push(jugadorAgregado);
-    console.log(jugadorAgregado.Carta1.carta);
-    
-    
+  var jugadorAgregado = new jugador(nombre, saldo);
+
+  jugadorAgregado.Carta1 = agregarCarta();
+  jugadorAgregado.Carta2 = agregarCarta();
+  jugadores.push(jugadorAgregado);
+  console.log(cartasAsignadas.length);
+  console.log(jugadorAgregado.Carta1);
+  console.log(jugadorAgregado.Carta2);
+
+
 }
 
 
 function agregarCarta() {
-    var asignada = true;
-    var numCartas=cartasAsignadas.length;
-    while (asignada) {
+  var asignada = false;
+  var contador = 0;
+  var numCartas = cartasAsignadas.length;
+  while (asignada === false) {
+    contador = 0;
+    numCartas = cartasAsignadas.length;
     var cartaJugadorAgregado = new Cartas();
-        
-        if(numCartas!==0){
-        
-        
-        for(var i=0;i<numCartas;i++) {
-            
-            if (cartasAsignadas[i].carta!==cartaJugadorAgregado.carta) {
-                cartasAsignadas.push(cartaJugadorAgregado);
-                
-                asignada = false;
-            }
+    if (numCartas === 0) {
+      cartasAsignadas.push(cartaJugadorAgregado);
+      asignada = true;
+    } else {
+      for (var i = 0; i < numCartas; i++) {
+        if (cartasAsignadas[i].carta !== cartaJugadorAgregado.carta) {
+          contador = contador + 1;
         }
-        }else{
-            cartasAsignadas.push(cartaJugadorAgregado);
-            break;
-        }
+      }
+      if (contador === numCartas) {
+        cartasAsignadas.push(cartaJugadorAgregado);
+        asignada = true;
+      }
     }
-    return cartaJugadorAgregado;
-
-
-
+  }
+  return cartaJugadorAgregado;
 }
+
+
 
 
 function $(query) {
 
-    return document.querySelector(query);
+  return document.querySelector(query);
 
 }
 
 /*-------------------------------------------------------------*/
 function reparticion() {
-    var carta1 = new Cartas();
-    var carta2 = new Cartas();
+  var carta1 = new Cartas();
+  var carta2 = new Cartas();
 
 
 }
 
-$("#btnAgregarJugador").onclick = function () {
-    var sNombre = $("#txtNombre").value;
-    var iDinero = $("#txtDinero").value;
-    var texto = document.createTextNode(sNombre + "-$" + iDinero);
-    var nodo = document.createElement("p").appendChild(texto);
-
-    add(sNombre, iDinero);
-
-    $("#divPantallaInicial").appendChild(nodo);
-};
 
 
-$("#btnComenzar").onclick = function () {
+$("#btnComenzar").onclick = function() {
 
-    if (jugadores.length > 1) {
+  if (jugadores.length > 1) {
 
 
-    }
+  }
 };
 
 
 
 function init() {
 
-    var iJ = Math.floor(Math.random() * 5 + 1);
-    $("#main").className = "main";
-//    $("#nose").className="main";
+  var iJ = Math.floor(Math.random() * 5 + 1);
+  $("#main").className = "main";
+  //    $("#nose").className="main";
 
 
 }
 
-$("#btnAceptar").onclick = function(event){
-  
+$("#btnAceptar").onclick = function(event) {
+
   apuestaMinima = $("#txtApuestaMinima").value;
-  if(apuestaMinima ==="undefined" || apuestaMinima < 1 ){
+  if (apuestaMinima === "undefined" || apuestaMinima < 1) {
     $("#txtApuestaMinima").className = "bordeRojo";
-  }else{
+  } else {
 
-  $("#divApuestaMinima").className = "oculto";
-  $("#divIngresoDeDatos").className = "divIngresoDeDatos";
-  $("#spanApuestaMinima").innerHTML = apuestaMinima;
-  $("#lblJugadores").innerHTML = "JUGADORES:";
+    $("#divApuestaMinima").className = "oculto";
+    $("#divIngresoDeDatos").className = "divIngresoDeDatos";
+    $("#spanApuestaMinima").innerHTML = apuestaMinima;
+    $("#lblJugadores").innerHTML = "JUGADORES:";
 
-}
+  }
 
 };
 
@@ -138,35 +118,24 @@ $("#btnAgregarJugador").onclick = function(event) {
   var error = false;
   if (sNombre === "" || sNombre === "undefined") {
     $("#txtNombre").className = "bordeRojo";
-    error=true;
+    error = true;
   }
   if (iDinero === "") {
     $("#txtDinero").className = "bordeRojo";
-    error=true;
+    error = true;
   }
-  if(!error){
-  var texto = document.createTextNode(sNombre + "-$" + iDinero);
-  var nodo = document.createElement("p");
-  nodo.appendChild(texto);
-  add(sNombre, iDinero);
-  $("#divPantallaInicial").appendChild(nodo);
-  $("#txtDinero").className = "bordeNormal";
-  $("#txtDinero").className = "bordeNormal";
+  if (!error) {
+    var texto = document.createTextNode(sNombre + "-$" + iDinero);
+    var nodo = document.createElement("p");
+    nodo.appendChild(texto);
+    add(sNombre, iDinero);
+    $("#divPantallaInicial").appendChild(nodo);
+    $("#txtDinero").className = "bordeNormal";
+    $("#txtDinero").className = "bordeNormal";
 
 
-  event.preventDefault();
-  $("#formIngresoDeDatos").reset();
-}
+    event.preventDefault();
+    $("#formIngresoDeDatos").reset();
+  }
 
 };
-
-
-
-
-
-
-
-
-
-
-
