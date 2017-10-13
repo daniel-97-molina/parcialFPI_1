727556e88f635450c298f70b508ee2dd2d907228
Daniel Molina <daniel.97.molin@gmail.com>
2017-10-13 02:01:38 -0600

    formulario valilado

--- scripts/jugador.js
+++ scripts/jugador.js
@@ -1,70 +1,82 @@
 var jugadores = [];
-var apuestaMinima=0;
+var apuestaMinima = 0;
 
-function jugador(nombre,saldo){
-this.jugando = false;
-this.nombre=nombre;
-this.saldo=saldo;
-    
-} 
+function jugador(nombre, saldo) {
+  this.jugando = false;
+  this.nombre = nombre;
+  this.saldo = saldo;
 
-jugador.prototype.apostar= function (monto){
-  saldo-=monto;  
+}
+
+jugador.prototype.apostar = function(monto) {
+  saldo -= monto;
 };
 
 
-function add(nombre,saldo){
-     jugadores.push(new jugador(nombre,saldo));
-    }
+function add(nombre, saldo) {
+  jugadores.push(new jugador(nombre, saldo));
+}
 
 
-function $(query){
-    return document.querySelector(query);
+function $(query) {
+  return document.querySelector(query);
 }
 
 /*-------------------------------------------------------------*/
 
+$("#btnAceptar").onclick = function(){
+  apuestaMinima = $("#txtApuestaMinima").value;
+  if(apuestaMinima == "undefined" || apuestaMinima < 1 ){
+    $("#txtApuestaMinima").className = "bordeRojo";
+  }else{
 
-$("#btnAgregarJugador").onclick=function (){
-    var sNombre=$("#txtNombre").value;
-    var iDinero=$("#txtDinero").value;
-    var texto = document.createTextNode(sNombre+"-$"+iDinero);
-    var nodo = document.createElement("p").appendChild(texto);
-    
-  add(sNombre,iDinero);
-   
+  $("#divApuestaMinima").className = "oculto";
+  $("#divIngresoDeDatos").className = "divIngresoDeDatos";
+  $("#spanApuestaMinima").innerHTML = apuestaMinima;
+  $("#lblJugadores").innerHTML = "JUGADORES:";
+}
+};
+
+$("#btnAgregarJugador").onclick = function() {
+
+  var sNombre = $("#txtNombre").value;
+  var iDinero = $("#txtDinero").value;
+  var error = false;
+  if (sNombre == "" || sNombre == "undefined") {
+    $("#txtNombre").className = "bordeRojo";
+    error=true;
+  }
+  if (iDinero == "") {
+    $("#txtDinero").className = "bordeRojo";
+    error=true;
+  }
+  if(!error){
+  var texto = document.createTextNode(sNombre + "-$" + iDinero);
+  var nodo = document.createElement("p");
+  nodo.appendChild(texto);
+  add(sNombre, iDinero);
   $("#divPantallaInicial").appendChild(nodo);
+  $("#txtDinero").className = "bordeNormal";
+  $("#txtDinero").className = "bordeNormal";
+}
 };
 
 
-$("#btnComenzar").onclick=function (){
-if(jugadores.length>1){
-    
- 
-    }
-};   
-    
 
 
-function init(){
-    
-    var iJ=Math.floor(Math.random()*5+1);
-    $("#botonJ"+iJ).className="noseeee";
-    
-    
+$("#btnComenzar").onclick = function() {
+  if (jugadores.length > 1) {
+
+
+  }
+};
+
+
+
+function init() {
+
+  var iJ = Math.floor(Math.random() * 5 + 1);
+  $("#botonJ" + iJ).className = "noseeee";
+
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- scripts/script3.js
+++ scripts/script3.js
@@ -1,5 +1,12 @@
 $("#btnAceptar").onclick = function(){
+  apuestaMinima = $("#txtApuestaMinima").value;
+  if(apuestaMinima == "undefined" || apuestaMinima < 1 ){
+    $("#txtApuestaMinima").className = "bordeRojo";
+  }else{
+
   $("#divApuestaMinima").className = "oculto";
   $("#divIngresoDeDatos").className = "divIngresoDeDatos";
-  $("#spanApuestaMinima").innerHTML = $("#txtApuestaMinima").value;
+  $("#spanApuestaMinima").innerHTML = apuestaMinima;
+  $("#lblJugadores").innerHTML = "JUGADORES:";
+}
 };
