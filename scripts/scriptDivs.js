825784a13af8fc96acd3f653fb482ea7fc57ac52
IrvinAyala <hypleyone@gmail.com>
2017-10-13 14:57:40 -0600

    script para los divs

---
+++ scripts/scriptDivs.js
@@ -0,0 +1,77 @@
+function posicionar(numJugadores) {
+  switch (numJugadores) {
+    case 2:
+      $("#div3").className = "divJugador";
+      $("#div8").className = "divJugador";
+      break;
+    case 3:
+      $("#div2").className = "divJugador";
+      $("#div4").className = "divJugador";
+      $("#div8").className = "divJugador";
+      break;
+    case 4:
+      $("#div2").className = "divJugador";
+      $("#div4").className = "divJugador";
+      $("#div7").className = "divJugador";
+      $("#div9").className = "divJugador";
+      break;
+    case 5:
+      $("#div1").className = "divJugador";
+      $("#div3").className = "divJugador";
+      $("#div5").className = "divJugador";
+      $("#div7").className = "divJugador";
+      $("#div9").className = "divJugador";
+      break;
+    case 6:
+      $("#div2").className = "divJugador";
+      $("#div4").className = "divJugador";
+      $("#div6").className = "divJugador";
+      $("#div7").className = "divJugador";
+      $("#div8").className = "divJugador";
+      $("#div10").className = "divJugador";
+      break;
+    case 7:
+      $("#div2").className = "divJugador";
+      $("#div3").className = "divJugador";
+      $("#div5").className = "divJugador";
+      $("#div6").className = "divJugador";
+      $("#div8").className = "divJugador";
+      $("#div9").className = "divJugador";
+      $("#div10").className = "divJugador";
+      break;
+    case 8:
+      $("#div1").className = "divJugador";
+      $("#div2").className = "divJugador";
+      $("#div3").className = "divJugador";
+      $("#div4").className = "divJugador";
+      $("#div6").className = "divJugador";
+      $("#div7").className = "divJugador";
+      $("#div8").className = "divJugador";
+      $("#div9").className = "divJugador";
+      break;
+    case 9:
+      $("#div1").className = "divJugador";
+      $("#div2").className = "divJugador";
+      $("#div3").className = "divJugador";
+      $("#div4").className = "divJugador";
+      $("#div6").className = "divJugador";
+      $("#div7").className = "divJugador";
+      $("#div8").className = "divJugador";
+      $("#div9").className = "divJugador";
+      $("#div10").className = "divJugador";
+      break;
+    case 10:
+      $("#div1").className = "divJugador";
+      $("#div2").className = "divJugador";
+      $("#div3").className = "divJugador";
+      $("#div4").className = "divJugador";
+      $("#div5").className = "divJugador";
+      $("#div6").className = "divJugador";
+      $("#div7").className = "divJugador";
+      $("#div8").className = "divJugador";
+      $("#div9").className = "divJugador";
+      $("#div10").className = "divJugador";
+
+      break;
+  }
+}
