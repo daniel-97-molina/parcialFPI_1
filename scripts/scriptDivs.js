659542711d792695c60e8b5665a974ab1a6519f1
Christian Zaldivar <herreraChristian1897@gmail.com>
2017-10-13 18:06:12 -0600

    antesDeArreglarRutas

--- scripts/scriptDivs.js
+++ scripts/scriptDivs.js
@@ -1,38 +1,47 @@
 function posicionar(numJugadores) {
   switch (numJugadores) {
     case 2:
-      habilitarDivs(3, 8);
+      habilitarDivs([3, 8]);
       break;
     case 3:
-      habilitarDivs(2, 4, 8);
+      habilitarDivs([2, 4, 8]);
       break;
     case 4:
-      habilitarDivs(2, 4, 7, 8);
+      habilitarDivs([2, 4, 7, 8]);
       break;
     case 5:
-      habilitarDivs(1, 3, 5, 7, 9);
+      habilitarDivs([1, 3, 5, 7, 9]);
       break;
     case 6:
-      habilitarDivs(2, 4, 6, 7, 8, 10);
+      habilitarDivs([2, 4, 6, 7, 8, 10]);
       break;
     case 7:
-      habilitarDivs(2, 3, 5, 6, 8, 9, 10);
+      habilitarDivs([2, 3, 5, 6, 8, 9, 10]);
       break;
     case 8:
-      habilitarDivs(1, 2, 3, 4, 6, 7, 8, 9);
+      habilitarDivs([1, 2, 3, 4, 6, 7, 8, 9]);
       break;
     case 9:
-      habilitarDivs(1, 2, 3, 4, 6, 7, 8, 9, 10);
+      habilitarDivs([1, 2, 3, 4, 6, 7, 8, 9, 10]);
       break;
     case 10:
-      habilitarDivs(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+      habilitarDivs([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
       break;
   }
 }
 
 function habilitarDivs(divs) {
   for (var i = 0; i < divs.length; i++) {
-    document.getElementById("\"" + "div" + divs[i] + "\"").className = "divJugador";
-    jugadores[i].div = "div" + divs[i];
+      var div=$("#div" + divs[i]);
+    div.className = "divJugador";
+    jugadores[i].div = div;
+   $("#div"+divs[i]+" h6").innerHTML=jugadores[i].nombre;
+   $("#div"+divs[i]+" h5").innerHTML=jugadores[i].saldo;
+        console.log(jugadores[i].carta1.generarRuta());
+        var ruta = jugadores[i].carta1.generarRuta();
+  $("#div"+divs[i]+" .divCarta1").style.backgroundImage="url("+ruta+")";
+   $("#div"+divs[i]+" .divCarta1").style.display="inline-block";
+
+       
   }
 }
